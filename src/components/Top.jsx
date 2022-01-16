6ca8565ce780f2bc7bafe737b1302812ff19bc37
gustave-studio <gustave0222@gmail.com>
2022-01-16 15:52:59 +0900

    ✨未来予測に関する本を追加

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -8,15 +8,21 @@ import EditorialDepartmentRecommendationsBox from './EditorialDepartmentRecommen
 
 const Top = function () {
   const [businessBooks, setbusinessBooks] = useState([]);
+  const [futurePredictionBooks, setFuturePredictionBooks] = useState([]);
 
   useEffect(() => {
     const businessBooksData = axios.get(process.env.REACT_APP_BUSINESS_BOOKS_URL);
+    const futurePredictionBooksData = axios.get(process.env.REACT_APP_FUTURE_PREDICTION_URL);
     // const businessBooksData = axios.get('https://api.gustave-studio.com/api/business_books?category=business');
     console.log('----rankingData');
     console.log(businessBooksData);
 
     businessBooksData.then((response) => {
       setbusinessBooks(response.data.business_books.map((item) => ({ title: item.title, description: item.description, asin: item.asin })));
+    });
+
+    futurePredictionBooksData.then((response) => {
+      setFuturePredictionBooks(response.data.business_books.map((item) => ({ title: item.title, description: item.description, asin: item.asin })));
     });
   }, []);
 
@@ -28,6 +34,17 @@ const Top = function () {
       <Grid container>
         <Grid item xs={1} />
         <Grid item xs={10}>
+          <div className="ranking_header">
+            <h1>
+              未来予測本
+            </h1>
+          </div>
+          <div className="ranking_container">
+            <h2>ビジネス書のおすすめ</h2>
+            <div style={{ display: 'flex', overflowX: 'auto' }} className="rankings">
+              { futurePredictionBooks.length ? <EditorialDepartmentRecommendationsBox rankingData={futurePredictionBooks} /> : <FaSpinner icon="spinner" className="spinner" />}
+            </div>
+          </div>
           <div className="ranking_header">
             <h1>
               編集部のおすすめ
